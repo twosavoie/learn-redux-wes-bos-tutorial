c722370f31c0a316092255d9575e7df89cf0a6df
lsavoie <twosavoie@gmail.com>
2023-07-19 14:21:49 -0500

    Initial commit beginning through creating components

---
+++ client/reducers/posts.js
@@ -0,0 +1,10 @@
+// a reducer takes in two things:
+// 1. teh action (infor about what happened)
+// 2. copy of current state
+
+function posts(state = [], action) {
+  console.log(state, action);
+  return state;
+}
+
+export default posts;
